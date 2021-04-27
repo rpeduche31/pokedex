35ef3be153acc55c0ac84efb07af58c3063016d8
Chester Peduche <chester.peduche@whitecloak.com>
2021-04-27 14:07:44 +0800

    adding loading screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LoadingScreen from '../screens/LoadingScreen';
 import Home from '../screens/Home';
@@ -10,18 +10,19 @@ import SearchScreen from '../screens/SearchScreen';
 
 const Stack = createStackNavigator();
 
-export default props => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        {/* <Stack.Screen component={LoadingScreen} name="LoadingScreen" /> */}
-        <Stack.Screen component={Home} name="Home" />
-        <Stack.Screen component={DetailedScreen} name="DetailedScreen" />
-        <Stack.Screen component={SearchScreen} name="SearchScreen" />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default (props) => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+				<Stack.Screen component={LoadingScreen} name="LoadingScreen" />
+				<Stack.Screen component={Home} name="Home" />
+				<Stack.Screen component={DetailedScreen} name="DetailedScreen" />
+				<Stack.Screen component={SearchScreen} name="SearchScreen" />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
